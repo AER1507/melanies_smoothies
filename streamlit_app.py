ff6b08e1ac7fe4bdc44d007836a6355370c54ab7
AER1507 <andreseruiz@eaton.com>
2026-05-26 15:51:48 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
- # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
-
-# Write directly to the app
 
 st.markdown(
     """
@@ -23,29 +21,23 @@ cnx = st.connection('snowflake')
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
 
-
-ingredients_list = st.multiselect('Choose up to 5 ingredients', my_dataframe, max_selections =5)
-
+ingredients_list = st.multiselect('Choose up to 5 ingredients', my_dataframe, max_selections=5)
 
 if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                 values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
-    
-    time_to_insert = st.button ('Submit Order')
- 
-     if time_to_insert:
+    time_to_insert = st.button('Submit Order')
+
+    if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
-        import requests
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-        st.text(smoothiefroot_response.json())
-
-   #New section to display  Smoothiefroot nutrition information
-        import requests  
-        smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-        st.text(smoothiefroot_response)
