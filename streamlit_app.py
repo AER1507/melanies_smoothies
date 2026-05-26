6dc6bc6dac8cb6c024cda00549d873430dd75e18
AER1507 <andreseruiz@eaton.com>
2026-05-26 15:01:14 -0600

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
-SMOOTHIES.PUBLIC.MY_UPLOADED_FILE# Import python packages
+ # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -17,13 +16,11 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-import streamlit as st
-
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-
-session = get_active_session()
+cnx = st.connection('snowflake')
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
 
 
@@ -34,7 +31,7 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '  
+        ingredients_string += fruit_chosen + ' '
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                 values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
